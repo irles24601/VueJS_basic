6074fc213e403fbabee9cd455063a012acc12d8e
Toshiki Isawa <jack_the_fool@outlook.com>
2020-11-22 12:07:15 +0900

    v-on

---
+++ section2/イベント処理の基本 v-on/main.js
@@ -0,0 +1,12 @@
+var app = new Vue({
+  el: '#app',
+	data: {
+  	now: ''
+  },
+  methods: {
+  	onclick: function() {
+    	// alert('onclick');
+      this.now = new Date().toLocaleString();
+    }
+  }
+})
